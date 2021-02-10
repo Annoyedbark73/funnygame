5293200e473b746f7258fd2dd33458d56bebea80
Annoyedbark73 <annoyedbark73@gmail.com>
2021-02-09 21:16:03 -0600

    YEAH BOIIIII (finally made movement and basic shooting
    i made the character move and be able to do cool things like shot

---
+++ top down Shooter/Assets/Scripts/C#/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float MoveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    public Camera cam;
+    Vector2 mousepos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        mousepos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+     void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * MoveSpeed * Time.fixedDeltaTime);
+        Vector2 lookDir = mousepos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y,lookDir.x) * Mathf.Rad2Deg - 90f ;
+        rb.rotation = angle;
+    }
+}
